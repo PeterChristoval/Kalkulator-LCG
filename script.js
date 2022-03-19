0d8f40c979d3888df2acec599afd2457f4b58421
PeterChristoval <peterchristoval@gmail.com>
2022-03-19 15:37:54 +0700

    First step of the calculation

---
+++ script.js
@@ -0,0 +1,22 @@
+const bAcak = document.querySelector('.bAcak');
+const tBody = document.querySelector('.tbody');
+const z0 = document.querySelector('.z0');
+const c = document.querySelector('.c');
+const a = document.querySelector('.a');
+const m = document.querySelector('.m');
+function hitung(e) {
+    e.preventDefault();
+    const Z = (parseInt(a.value) * parseInt(z0.value) + parseInt(c.value)) % parseInt(m.value);
+    tBody.innerHTML += `<tr>
+                <td>${1}</td>
+                <td>${z0.value}</td>
+                <td>Z1 = (${a.value} * ${z0.value} + ${c.value}) mod ${m.value} = ${Z}</td>
+                <td>U1 = ${Z} / ${m.value} = ${Z / parseInt(m.value) }</td>
+            </tr>`
+    for (let i = 1; i < bAcak.value; i++) {
+        tBody.innerHTML += `<tr>
+            <td>${i + 1}</td>
+            <td>o</td>
+        </tr>`
+    }
+}
